6d59676f99ad5ff8da786cc18d352c84cde67723
RawabeT <rawabe.alshduki@tuwaiq.edu.sa>
2021-05-16 21:47:44 +0300

    Enimies creation

--- gameProject.js
+++ gameProject.js
@@ -4,7 +4,7 @@ let context = canvas.getContext("2d");
 let rightPress = false;
 let leftPress = false;
 let enterPress = false;
-let count = 0;
+let score = 0;
 
 let player = {
     x: 250, // position
@@ -37,35 +37,38 @@ function update() {
         }
     }
 
+    enemiesMove()
     drawblock()
     drawPlyer()
-    score()
+    scoreDisplay()
     shooting()
     drawShots()
+    drawEnemies()
     requestAnimationFrame(update);
 }
 
-function score() {
+
+function scoreDisplay() {
     context.fillStyle = 'blue';
     context.font = '20px Verdana';
-    context.fillText("Score : " + count, 20, 20);
+    context.fillText("Score : " + score, 20, 20);
 }
 
 let blocks = [];
 let block = {
     total: 2,
-    x: 50 + Math.random() * 100,
-    y: 50 + Math.random() * 100,
-    h: 50,
-    w: 100
+    x: 50 + Math.random() * 250,
+    y: 50 + Math.random() * 300,
+    w: 100,
+    h: 50
 }
 
 function drawblock() {
     blocks.push(context.fillStyle = 'red',
-        context.fillRect(block.x, block.y, block.w, block.y))
+        context.fillRect(block.x, block.y, 100, 50))
 
     blocks.push(context.fillStyle = 'red',
-        context.fillRect(block.x + 200, block.y + 100, block.w, block.y))
+        context.fillRect(block.x + 200, block.y - 30, 100, 50))
 }
 
 let shot = 10
@@ -88,6 +91,41 @@ function shooting() {
         }
     }
 }
+
+let enemies = []
+let enemy = {
+    x: 50,
+    y: 50,
+    h: 50,
+    w: 50,
+    speed: 2
+}
+
+function drawEnemies() { // draw enemies
+    if (enemies.length) {
+        for (let index = 0; index < enemies.length; index++) {
+            context.fillStyle = 'orange'
+            context.fillRect(enemies[index][0], enemies[index][1], enemies[index][2], enemies[index][3]);
+            console.log("enm")
+        }
+    }
+
+    for (let index = 0; index < 5; index++) {
+        enemies.push([enemy.x, enemy.y, enemy.h, enemy.w])
+        enemy.x += 100
+    }
+}
+
+function enemiesMove() {
+    for (let index = 0; index < enemies.length; index++) {
+        if (enemies[index][1] > 11) {
+            enemies[index][1] += 10;
+        } else if (enemies[index][1] < 10) {
+            enemies.splice(index, 1); // if shooting end first round remove
+        }
+    }
+}
+
 
 document.addEventListener('keydown', (e) => {
 
